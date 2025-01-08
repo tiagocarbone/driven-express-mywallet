6bb3495414ea8a70a029cf35c9242b4fc380a879
Tiago Carbone <carbonetiago@id.uff.br>
2025-01-08 10:27:12 -0300

    conecta banco e setando middlewares, controllers

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,3 +1,7 @@
 export async function getFunction(req, res) {
     res.send("oieeee")
 }
+
+export async function signUp(req, res){
+    res.send("sign-up")
+}
--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
-import {getFunction}  from "../controllers/authController.js";
+import {getFunction, signUp}  from "../controllers/authController.js";
+import userSchemaValidationMiddleware from "../middlewares/userSchemaValidationMiddleware.js";
 
 const authRouter = Router();
 authRouter.get("/", getFunction);
+authRouter.post("/sign-up", userSchemaValidationMiddleware, signUp)
 
 export default authRouter;
